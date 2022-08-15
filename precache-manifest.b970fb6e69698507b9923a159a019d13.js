6e9ad7907db8b8a0b21de5aac587ccbd9aa447e4
wzwdream <96275591+wzwdream@users.noreply.github.com>
2022-08-15 07:31:29 +0000

    Deploying to gh-pages from @ wzwdream/turntable@c847f64445c58c1f7193fd7a41f2a139fc556dcb 🚀

---
+++ precache-manifest.b970fb6e69698507b9923a159a019d13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eab8ae3f7c7ad0602e09",
+    "url": "/static/css/main.8b47487c.chunk.css"
+  },
+  {
+    "revision": "eab8ae3f7c7ad0602e09",
+    "url": "/static/js/main.eab8ae3f.chunk.js"
+  },
+  {
+    "revision": "2780bb8b722368246aba",
+    "url": "/static/js/1.2780bb8b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "106d77a99c9e3631c905ba57cea320a5",
+    "url": "/index.html"
+  }
+];
