5c3f4cf571b58f6624cfdc3f816274632110bf4e
wzwdream <96275591+wzwdream@users.noreply.github.com>
2022-08-15 07:40:44 +0000

    Deploying to gh-pages from @ wzwdream/turntable@b768bfed5d51b84d702addb49b5a067864b82517 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b970fb6e69698507b9923a159a019d13.js"
+  "./precache-manifest.f73fe63fca72426b1b54736337f513f0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
